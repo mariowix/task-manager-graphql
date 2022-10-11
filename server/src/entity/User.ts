296247215c61594610ef761b4cbbd2c6cddb456f
Mario Mixtega <mario@kabily.com>
2022-10-10 20:16:32 -0500

    Use entity as response object for graphql
    Using some decorators we can use the same structure for both the orm and graphql

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,18 +1,22 @@
+import { Field, Int, ObjectType } from "type-graphql"
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
 
 @Entity('user')
+  @ObjectType()
 export class User extends BaseEntity {
 
     @PrimaryGeneratedColumn()
+    @Field(() => Int)
     id: number
 
+  @Field()
     @Column()
     name: string
 
     @Column()
+    @Field()
     email: string
 
     @Column()
     password: string
-
 }
