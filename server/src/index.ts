dd9407b3d0ec13af016aa1e6a986a0e81a4d6d9d
Mario Mixtega <mario@kabily.com>
2022-10-10 21:20:25 -0500

    Pass res and req into the context

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,10 @@ dotenv.config();
 import 'module-alias/register';
 
 import express from 'express';
+import Fingerprint from 'express-fingerprint';
 import { AppDataSource } from "./data-source"
 import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import { UserResolver } from '@resolvers';
+import { createSchema } from '../utils';
 
 /* Wrap everything into a main function with async capabilities
    to use "await"
@@ -15,11 +15,13 @@ import { UserResolver } from '@resolvers';
 const main = async () => {
   await AppDataSource.initialize()
   const app = express();
-
+  // I added a fingerprint of the browser as part of the hashing for the JWT
+  // by doing that, the JWT would work only if it was generated on the same
+  // machine that it was generated (Fun)
+  app.use(Fingerprint())
   const server = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [UserResolver]
-    })
+    schema: await createSchema(),
+    context: ({ res, req }) => ({ res: res, req: req }),
   });
 
   await server.start()
