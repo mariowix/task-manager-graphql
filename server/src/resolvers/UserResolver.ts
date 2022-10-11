a8b1329d8a67c81935d13f646199d7d88ca36fac
Mario Mixtega <mario@kabily.com>
2022-10-10 20:29:17 -0500

    Document signup operation

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,6 +1,10 @@
 import { User } from "@entities";
 import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { hash, genSaltSync } from 'bcryptjs';
 
+/**
+ * Input type for signing up new users
+ */
 @InputType()
 class SignupInput {
   @Field()
@@ -16,13 +20,33 @@ class SignupInput {
 
 @Resolver()
 export default class UserResolver {
+  /**
+   * Mutation operation for create a new user
+   *
+   * @param {SignupInput} newUser - The new user info
+   * @param {string} newUser.name - The new user name
+   * @param {string} newUser.email - The new user email
+   * @param {string} newUser.password - The new user password
+   * @returns {Promise<User>} - The new user created
+   *
+   */
   @Mutation(() => User)
   async signup(
     @Arg("newUser") { name, email, password }: SignupInput
   ) {
     //TODO: add validation
 
-    return User.create({ name, email, password }).save();
+    // Check if the user exitsts
+    const user = await User.findOne({ where: { email } });
+
+    // If the user exists, we return an error, can't register the same user twice
+    if (user) throw Error("Email is already registered");
+
+    // Hash the password to store the hash instead of the plain password
+    const passwordHased = await hash(password, genSaltSync())
+
+    // Return the newly created user
+    return User.create({ name, email, password: passwordHased }).save();
   }
 
   @Query(() => String)
