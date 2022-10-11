67f3e93a8930aee5f8f83bcbb14bf3e06e607356
Mario Mixtega <mario@kabily.com>
2022-10-10 20:16:45 -0500

    Implement signup mutation

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,7 +1,19 @@
-import { Query, Resolver } from "type-graphql";
+import { User } from "@entities";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export default class UserResolver {
+  @Mutation(() => User)
+  async signup(
+    @Arg("name") name: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+  ) {
+    //TODO: add validation
+
+    return User.create({ name, email, password }).save();
+  }
+
   @Query(() => String)
   dummy() {
     return 'hello world'
