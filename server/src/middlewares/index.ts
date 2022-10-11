bd5456d56f9ae388909525664aeb22eb9cee990a
Mario Mixtega <mario@kabily.com>
2022-10-10 23:11:03 -0500

    Add middleware index file

---
+++ server/src/middlewares/index.ts
@@ -0,0 +1,9 @@
+import { CheckAuthenticated } from "./CheckAuthenticated";
+import { CheckAuthorized } from "./CheckAuthorized";
+import { ValidateInputs } from './ValidateInputs';
+
+export {
+  CheckAuthenticated,
+  CheckAuthorized,
+  ValidateInputs
+}
