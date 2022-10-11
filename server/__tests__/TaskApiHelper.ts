1b916a56bbbbceb6dbbedd3e3f0f3f53d3f4e542
Mario Mixtega <mario@kabily.com>
2022-10-10 23:13:25 -0500

    Add task helper methods

--- server/__tests__/TaskApiHelper.ts
+++ server/__tests__/TaskApiHelper.ts
@@ -68,4 +68,57 @@ export class TaskApiHelper {
     })
   }
 
+  cleanToken() {
+    this.req = {
+      fingerprint: {
+        hash: "dummy"
+      }
+    }
+  }
+
+  setToken(token: string) {
+    this.req.headers = { authorization: `Bearer ${token}` }
+  }
+  private createTaskMutation = `
+  mutation($description: String!, $title: String!) {
+    createTask(description: $description, title: $title) {
+      title,
+      id,
+      status
+    }
+  }`;
+
+  createTask(title: string, description: string) {
+    return this._gCall({
+      source: this.createTaskMutation,
+      variableValues: {
+        title,
+        description
+      }
+    })
+  }
+
+  private updateTaskMutation = `
+  mutation($updateTaskId: Int!, $newData: UpdateTaskInput!) {
+    updateTask(id: $updateTaskId, newData: $newData) {
+      id,
+      title,
+      description,
+      status
+    }
+  }`;
+
+  updateTask(updateTaskId: number, title?: string, description?: string, status?: string) {
+    return this._gCall({
+      source: this.updateTaskMutation,
+      variableValues: {
+        updateTaskId,
+        newData: {
+          title,
+          description,
+          status
+        }
+      }
+    })
+  }
 }
