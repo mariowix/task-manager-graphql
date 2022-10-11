1d672bd9a09ca401de4b4fd4b48622a303b4e192
Mario Mixtega <mario@kabily.com>
2022-10-10 21:21:01 -0500

    Add the login mutation

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,6 +1,10 @@
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
+import { hash, genSaltSync, compare } from 'bcryptjs';
+import { AuthenticationError } from "apollo-server-express";
+import { sign } from 'jsonwebtoken';
+
 import { User } from "@entities";
-import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
-import { hash, genSaltSync } from 'bcryptjs';
+import { GlobalContextType } from "../GlobalContextType";
 
 /**
  * Input type for signing up new users
@@ -15,7 +19,13 @@ class SignupInput {
 
   @Field()
   email: string
+}
 
+// Response for the login mutation
+@ObjectType()
+export class LoginResponse {
+  @Field()
+  accessToken: string
 }
 
 @Resolver()
@@ -28,11 +38,11 @@ export default class UserResolver {
    * @param {string} newUser.email - The new user email
    * @param {string} newUser.password - The new user password
    * @returns {Promise<User>} - The new user created
-   *
    */
   @Mutation(() => User)
   async signup(
-    @Arg("newUser") { name, email, password }: SignupInput
+    @Arg("newUser")
+    { name, email, password }: SignupInput
   ) {
     //TODO: add validation
 
@@ -47,6 +57,57 @@ export default class UserResolver {
 
     // Return the newly created user
     return User.create({ name, email, password: passwordHased }).save();
+  }
+
+  /**
+  * Mutation operation for login an user.
+  * @param {string} email - The email of the user.
+  * @param {string} password - The password of the user.
+  * @returns {Promise<LoginResponse>} - The response containing an accessToken
+  */
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email")
+    email: string,
+
+    @Arg("password")
+    password: string,
+
+    @Ctx()
+    { req: { fingerprint: { hash: fingerprint } }, res }: GlobalContextType
+  ) {
+    // Look for the user to see if it even exists
+    const user = await User.findOne({ where: { email } });
+
+    // If the user does not exists we return an error
+    if (!user)
+      throw new AuthenticationError("Email or password invalid");
+
+    // Check if the password produces the same hash
+    const isValidPass = await compare(password, user.password);
+
+    // If not, we return an error
+    if (!isValidPass)
+      throw new AuthenticationError("Email or password invalid");
+
+    // Generate accessToken
+    const accessToken = sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET! + fingerprint, { expiresIn: '15m' })
+
+    // Set a cookie to store the refresh token
+    res.cookie(
+      'tartk',
+      sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET! + fingerprint, { expiresIn: '15m' }),
+      {
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+      }
+    );
+
+    // Return the accessToken
+    return {
+      accessToken
+    }
   }
 
   @Query(() => String)
