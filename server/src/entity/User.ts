6433cdb1bb5a28ceb191643f87ce4251d98580de
Mario Mixtega <mario@kabily.com>
2022-10-10 20:12:16 -0500

    Add missing import

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
 
 @Entity('user')
 export class User extends BaseEntity {
