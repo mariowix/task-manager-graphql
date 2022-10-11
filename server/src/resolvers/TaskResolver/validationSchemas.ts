b9468f573dbfa358047693b1b4d10ff5872e0474
Mario Mixtega <mario@kabily.com>
2022-10-10 23:30:40 -0500

    Fix schemas for task resolver

--- server/src/resolvers/TaskResolver/validationSchemas.ts
+++ server/src/resolvers/TaskResolver/validationSchemas.ts
@@ -4,15 +4,15 @@ import { TASK_STATUSES } from "../../constants";
 // Input validation schema for create task
 export const CreateTaskSchema = yup.object().shape({
   title: yup.string().required().min(3).max(255),
-  description: yup.string().min(3).max(255)
+  description: yup.string().max(255)
 });
 
 // Input validation schema for update task
 export const UpdateTaskSchema = yup.object().shape({
   id: yup.number(),
   newData: yup.object().shape({
-    title: yup.string().required().min(3).max(255),
-    description: yup.string().required().min(3).max(255),
-    status: yup.string().oneOf(Object.values(TASK_STATUSES))
+    title: yup.string().notRequired().max(255),
+    description: yup.string().notRequired().min(3).max(255),
+    status: yup.string().notRequired().oneOf(Object.values(TASK_STATUSES))
   })
 });
